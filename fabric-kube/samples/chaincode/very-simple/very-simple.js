530443411073a651106fba43d2c2666651872648
Pim Otte <otte.pim@gmail.com>
2020-10-16 15:24:43 +0200

    Trying to get private collection

--- fabric-kube/samples/chaincode/very-simple/very-simple.js
+++ fabric-kube/samples/chaincode/very-simple/very-simple.js
@@ -28,6 +28,13 @@ const Chaincode = class {
   async ping() {
     logger.info("ping called", "");
     const answer = { ping: 'pong' };
+
+    // get the transient maplet transientMarble = stub.getTransient();// convert into buffer
+    // var buffer = new Buffer(transientMarble.map.conversation.value.toArrayBuffer());// from buffer into string
+    // var JSONString = buffer.toString("utf8");// from json string into object
+    var JSONObject = JSON.parse(JSONString);
+    await stub.putPrivateData("collectionAdres", "1231123123", answer);
+
     return Buffer.from(JSON.stringify(answer), 'utf8');
   }
 };
